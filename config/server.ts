9ec47e2acaf3ea0c978211bbe9cfb71f3af6d09c
Adam Karski <zbigniew.karski@gmail.com>
2025-05-27 16:33:44 +0200

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['someRandomKey1', 'someRandomKey2']),
   },
   url: env('URL'),
   proxy: true
